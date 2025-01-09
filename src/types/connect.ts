7417898a4537ce45c89bf104a8a8266e0797c6fd
alfisyahrinn <sahrin201@gmail.com>
2025-01-09 12:00:26 +0700

    block connection

--- src/types/connect.ts
+++ src/types/connect.ts
@@ -1,1 +1,2 @@
-export const URL_API = "https://webgis-backend-production.up.railway.app/api"
+// export const URL_API = "https://webgis-backend-production.up.railway.app/api"
+export const URL_API = "https://asdsa"
